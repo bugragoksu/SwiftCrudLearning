acc267eb25784706f95ed68448d28ec21342f46c
bugragoksu <bugragoksu2@gmail.com>
2021-12-07 15:24:00 +0300

    init

---
+++ GithubAPI/GithubAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubAPIApp.swift
+//  GithubAPI
+//
+//  Created by USER on 7.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct GithubAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
